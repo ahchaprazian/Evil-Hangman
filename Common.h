08548487e59d1db4ccb05da6f16c2f3779033763
Alexan Chaprazian <53905350+AlexChaprazian@users.noreply.github.com>
2021-09-29 18:30:56 -0400

    Delete Common.h

--- Common.h
+++
@@ -1,9 +0,0 @@
-#ifndef COMMON_H
-#define COMMON_H
-
-typedef enum status { FAILURE, SUCCESS } Status;
-typedef enum boolean { FALSE, TRUE } Boolean;
-typedef enum largerString { LEFT=-1, SAME, RIGHT} LargerString;
-typedef void* Item_ptr;
-
-#endif
